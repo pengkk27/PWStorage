31182582779bc0a38449a1f41686937c71a99e65
pengkk27 <1481128018@qq.com>
2021-10-18 21:17:01 +0800

    定义Mapper中的操作

--- src/main/java/com/pengkk27/pwstorage/dao/CategoryMapper.java
+++ src/main/java/com/pengkk27/pwstorage/dao/CategoryMapper.java
@@ -1,7 +1,18 @@
 package com.pengkk27.pwstorage.dao;
 
+import com.pengkk27.pwstorage.entity.Category;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface CategoryMapper {
+
+    void addCategory(Category category);
+
+    void updateCategory(Category category);
+
+    List<Category> getAllCategory();
+
+    void deleteCategoryById(String categoryId);
 }
